6d3dc4be9c774de98d37446821bf5d7d7467b334
Muhammad Daniyal <daniyalghani159@gmail.com>
2024-05-14 10:22:40 +0500

    add cors

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 const connectDB = require("./db/connect_db.js");
 const adminRouter = require("./routes/admin.js");
 const router = require("./routes/route.js");
 const authenticationMiddleware = require("./middleware/auth.js");
+
+app.use(cors());
 
 const PORT = 3000;
 connectDB();
